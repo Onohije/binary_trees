cd358f992096aebfe8d55b1c0f83126b6c0afb8e
fessycode <fessydoris@gmail.com>
2023-08-31 02:23:23 -0700

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,18 +5,17 @@
  * @parent: Pointer to a parent node to the new node to be created.
  * @value: The value/data of the node to be created.
  * Return: pointer to the newly created node.
- */
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL) {
-        return NULL; // Memory allocation failed
-    }
+	if (new_node == NULL)
+		return (NULL);
+	new_node->val = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
 
-    new_node->val = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-
-    return (new_node);
+	return (new_node);
 }
